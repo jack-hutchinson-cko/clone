95a64e8b3d7b997f8a9ec80827524386bd57f9b5
Serhii Shvets <84973222+serhii-shvets@users.noreply.github.com>
2021-06-10 13:02:19 +0300

    #12 add information table component (#48)

---
+++ components/Table/Table.tsx
@@ -0,0 +1,1 @@
+export { Table, TableHead, TableHeadSticky, TableBody, TableCell, TableRow } from '@cko/primitives'
---
+++ components/Table/index.tsx
@@ -0,0 +1,4 @@
+import { Table, TableHead, TableHeadSticky, TableBody, TableCell, TableRow } from './Table'
+
+export default Table
+export { TableHead, TableHeadSticky, TableBody, TableCell, TableRow }
