4416e1ff108cd7b32384414f101e597d80f87426
oviktorovlohika <84976693+oviktorovlohika@users.noreply.github.com>
2021-07-08 15:56:26 +0300

    fix shadow style  card component (#190)

--- components/Card/Card.styles.ts
+++ components/Card/Card.styles.ts
@@ -6,12 +6,12 @@ export const CardWrapper = styled(Flex)`
   flex-direction: column;
   padding: 32px;
   border: 1px solid ${({ theme }) => theme.colors.greyLight};
-  transition: box-shadow 0.3s, border-color 0.7s;
+  transition: box-shadow 0.1s, border-color 0.7s;
   border-radius: 8px;
   cursor: pointer;
   height: 100%;
   &:hover {
-    box-shadow: ${({ theme }) => theme.shadows.focusLight[2]};
+    box-shadow: 0px 8px 16px rgba(12, 17, 66, 0.12);
     border-color: ${({ theme }) => theme.colors.blueTertiary};
   }
   svg {
