fd883b64accb26bb01372dd1d9d225572c95534e
pzhovnir378 <84974969+pzhovnir378@users.noreply.github.com>
2021-07-09 10:39:45 +0300

    #107 Added analytic scripts (#194)

--- components/Head/Head.tsx
+++ components/Head/Head.tsx
@@ -1,21 +1,28 @@
 import { FC } from 'react';
 import Head from 'next/head';
 
+import GoogleTags from './Trackers/GoogleTags';
+import Boomerang from './Trackers/Boomerang';
+import HeapIO from './Trackers/HeapIO';
+import HotJar from './Trackers/HotJar';
+
 type Props = {
   title?: string;
 };
 
-const PageHead: FC<Props> = ({ title }) => (
+const PageHead: FC<Props> = ({ title = 'Checkout.com' }) => (
   <Head>
     <title>{title} - Docs</title>
     <link rel="shortcut icon" href="/favicon.ico" />
     <meta charSet="utf-8" />
     <meta name="viewport" content="width=device-width" />
+    <GoogleTags id={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
+    <Boomerang id={process.env.NEXT_PUBLIC_BOOMERANG_ID} />
+    <HeapIO
+      id={process.env.NEXT_PUBLIC_HEAP_IO_ID}
+      account={process.env.NEXT_PUBLIC_HEAP_IO_ACCOUNT}
+    />
+    <HotJar id={process.env.NEXT_PUBLIC_HOT_JAR_ID} />
   </Head>
 );
-
-PageHead.defaultProps = {
-  title: 'Checkout.com',
-};
-
 export default PageHead;
---
+++ components/Head/Trackers/HeapIO.tsx
@@ -0,0 +1,48 @@
+import { FC } from 'react';
+
+type Props = {
+  id?: string;
+  account?: string;
+};
+
+const HeapIO: FC<Props> = ({ id, account }) => (
+  <>
+    <script
+      type="text/javascript"
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+            heap.load("${id}", {
+                secureCookie: true
+            });
+            heap.addUserProperties({ Account: '${account}' });
+        `,
+      }}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+              (function() {
+                var quarters = 0;
+                var scrollHeight, quarterHeight, scrollDistance, divisible, scrollPercent;
+                document.addEventListener("scroll", function() {
+                    scrollHeight = document.documentElement.scrollHeight - window.innerHeight;
+                    quarterHeight = scrollHeight / 4;
+                    scrollDistance = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+                    divisible = Math.trunc(scrollDistance / quarterHeight);
+                    if (quarters < divisible && divisible !== Infinity) {
+                        scrollPercent = divisible * 25;
+                        heap.track('Scroll Depth', {
+                            percent: scrollPercent
+                        });
+                        quarters++;
+                    }
+                });
+            }());
+        `,
+      }}
+    />
+  </>
+);
+
+export default HeapIO;
