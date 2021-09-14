8f5e716eeb2598ed2d342ac51e4ec3533a249054
oviktorovlohika <84976693+oviktorovlohika@users.noreply.github.com>
2021-09-14 12:02:10 +0300

    add hover animation on country card link (#463)

--- components/CountryCardLink/CountryCardLink.styles.ts
+++ components/CountryCardLink/CountryCardLink.styles.ts
@@ -10,7 +10,14 @@ export const Wrapper = styled.a`
   border-radius: 8px;
   border: 1px solid ${({ theme }) => theme.colors.border};
   cursor: pointer;
+  transition: box-shadow 0.1s, border-color 0.7s;
+
+  &:hover {
+    box-shadow: ${({ theme }) => theme.shadows.light};
+    border-color: ${({ theme }) => theme.colors.borderHighlight};
+  }
 `;
+
 export const Title = styled.h4`
   font-weight: 500;
   line-height: 24px;
