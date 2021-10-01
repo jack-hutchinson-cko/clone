773ab90e7a62a8fe82514c83e4fa20abe335d04a
Serhii Shvets <84973222+serhii-shvets@users.noreply.github.com>
2021-10-01 11:56:35 +0300

    add local flag for feature flagging (#525)

--- hoc/withLDProvider/withLDProvider.tsx
+++ hoc/withLDProvider/withLDProvider.tsx
@@ -3,8 +3,17 @@ import { LDProvider } from 'launchdarkly-react-client-sdk';
 
 const withLDProvider = <T,>(Component: FC<T>): FC<T> => {
   return (props) => {
+    let ldProps = {};
+
+    if (process.env.NEXT_PUBLIC_DOCS_INTEGRATION_BUILDER_FRAMES === 'true') {
+      ldProps = { flags: { docsIntegrationBuilderFrames: true } };
+    }
+
     return (
-      <LDProvider clientSideID={String(process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID)}>
+      <LDProvider
+        clientSideID={String(process.env.NEXT_PUBLIC_LAUNCHDARKLY_CLIENT_ID)}
+        {...ldProps}
+      >
         <Component {...props} />
       </LDProvider>
     );
