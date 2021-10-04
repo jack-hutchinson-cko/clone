020867e9332ed83259088129de7afda03ef17850
Andrii Maziein <84718601+amaziein@users.noreply.github.com>
2021-10-04 16:59:42 +0300

    Fix global styles (#573)

--- components/Text/Text.styles.ts
+++ components/Text/Text.styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Text = styled.p`
   color: ${({ theme }) => theme.colors.baseLight};
   font-size: 16px;
-  margin: 24px 0;
+  margin: 16px 0;
   line-height: 24px;
+  font-weight: 300;
 `;
--- components/TextHeading/TextHeading.styles.ts
+++ components/TextHeading/TextHeading.styles.ts
@@ -5,7 +5,7 @@ export const TextHeadingOne = styled.h1`
   font-size: 56px;
   font-weight: 600;
   line-height: 64px;
-  margin: 32px 0;
+  margin: 32px 0 16px 0;
 `;
 
 export const TextHeadingTwo = styled.h2`
@@ -13,19 +13,19 @@ export const TextHeadingTwo = styled.h2`
   font-size: 40px;
   font-weight: 600;
   line-height: 48px;
-  margin: 26px 0;
+  margin: 32px 0 16px 0;
 `;
 
 export const TextHeadingThree = styled.h3`
   color: ${({ theme }) => theme.colors.base};
   font-size: 24px;
   line-height: 32px;
-  margin: 16px 0;
+  margin: 32px 0 16px 0;
 `;
 
 export const TextHeadingFour = styled.h4`
   color: ${({ theme }) => theme.colors.base};
   font-size: 18px;
   line-height: 24px;
-  margin: 16px 0;
+  margin: 32px 0 16px 0;
 `;
