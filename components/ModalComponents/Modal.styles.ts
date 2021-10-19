29ef990dc2c78bf985abcd0a4442d54e728d24e6
Serhii Shvets <84973222+serhii-shvets@users.noreply.github.com>
2021-10-19 10:53:44 +0300

    692-721 expanded image overlay issues (#742)
    * add padding for the mobile and tablet view
    
    * some refactoring

--- components/ModalComponents/Modal.styles.ts
+++ components/ModalComponents/Modal.styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components';
+import { Breakpoints, createBreakpointTo, SIZE } from 'constants/screen';
+import { spacing } from 'constants/spacingSize';
 
 export const ModalRoot = styled.div<{ isOpen?: boolean }>`
   display: flex;
@@ -6,17 +8,26 @@ export const ModalRoot = styled.div<{ isOpen?: boolean }>`
   height: 100%;
   width: 100%;
   background-color: ${({ theme }) => theme.colors.modalBackground};
-  padding: 80px 165px;
+  padding: ${spacing.s50}px;
   cursor: pointer;
   position: fixed;
   top: 0;
   left: 0;
   z-index: 1;
+
+  @media ${Breakpoints.MOBILE} {
+    padding: ${spacing.s60}px ${spacing.s90}px;
+  }
+
+  @media ${createBreakpointTo(SIZE.SM)} {
+    padding: ${spacing.s80}px ${spacing.s50}px;
+  }
 `;
 
 export const ContentWrapper = styled.div`
+  position: relative;
   display: flex;
-  max-width: 100%;
+  width: 100%;
 `;
 
 export const IconClose = styled.span`
@@ -41,5 +52,10 @@ export const IconClose = styled.span`
   }
   &:after {
     transform: rotate(-45deg);
+  }
+
+  @media ${createBreakpointTo(SIZE.SM)} {
+    right: ${spacing.s50}px;
+    top: ${spacing.s50}px;
   }
 `;
