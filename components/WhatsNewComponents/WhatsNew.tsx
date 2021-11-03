093d59c24e88ab0dfc347c265446daf3f4a36e98
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-11-03 16:26:46 +0000

    Use timeout again

--- components/WhatsNewComponents/WhatsNew.tsx
+++ components/WhatsNewComponents/WhatsNew.tsx
@@ -1,4 +1,5 @@
 import { useEffect, FC } from 'react';
+
 import { NOTICEABLE_WIDGET_ID } from 'constants/keys';
 import { WhatsNewWidget } from './WhatsNew.styles';
 import { WindowWithNoticeableType } from './types';
@@ -6,15 +7,18 @@ import { WindowWithNoticeableType } from './types';
 const WhatsNew: FC = () => {
   useEffect(() => {
     const windowWithNoticeable = window as WindowWithNoticeableType;
-    if (windowWithNoticeable.noticeable) {
-      windowWithNoticeable.noticeable.render('widget', NOTICEABLE_WIDGET_ID);
-    }
+    setTimeout(() => {
+      if (windowWithNoticeable.noticeable) {
+        windowWithNoticeable?.noticeable.render('widget', NOTICEABLE_WIDGET_ID);
+      }
+    }, 200);
+
     return () => {
       if (windowWithNoticeable.noticeable) {
         windowWithNoticeable.noticeable.destroy('widget', NOTICEABLE_WIDGET_ID);
       }
     };
-  });
+  }, []);
 
   return <WhatsNewWidget id="noticeable-widget" />;
 };
