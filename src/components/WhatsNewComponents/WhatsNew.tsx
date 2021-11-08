4ac954140b4512b62883c3ac291e2275ffa136f8
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-11-08 16:52:16 +0000

    Add fallback UI for no noticeable

--- src/components/WhatsNewComponents/WhatsNew.tsx
+++ src/components/WhatsNewComponents/WhatsNew.tsx
@@ -1,20 +1,26 @@
-import { useEffect, FC } from 'react';
+import { useEffect, useState, FC } from 'react';
 
 import { NOTICEABLE_WIDGET_ID } from 'src/constants/keys';
+import TipBox from '../TipBox';
 import { WhatsNewWidget } from './WhatsNew.styles';
 import { WindowWithNoticeableType } from './types';
 
 const WhatsNew: FC = () => {
+  const [hasWindow, setWindow] = useState<boolean>(false);
+
   useEffect(() => {
     let intervalId: any;
     const windowWithNoticeable = window as WindowWithNoticeableType;
 
     if (windowWithNoticeable.noticeable) {
+      setWindow(true);
       windowWithNoticeable.noticeable.render('widget', NOTICEABLE_WIDGET_ID);
     } else {
+      setWindow(false);
       console.warn('Noticeable is missing, will retry in 100ms');
       intervalId = setInterval(() => {
         if (windowWithNoticeable.noticeable) {
+          setWindow(true);
           windowWithNoticeable.noticeable.render('widget', NOTICEABLE_WIDGET_ID);
           clearInterval(intervalId);
         }
@@ -27,7 +33,15 @@ const WhatsNew: FC = () => {
     };
   }, []);
 
-  return <WhatsNewWidget id="noticeable-widget" />;
+  return hasWindow ? (
+    <WhatsNewWidget id="noticeable-widget" />
+  ) : (
+    <TipBox title="Short error message descriptor here" variant="note">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Accumsan orci interdum id lectus
+      cursus maecenas semper ante et. Nisi accumsan sed pharetra nunc urna. Id lobortis in duis ut
+      quis suspendisse massa. Congue nulla venenatis, tortor, tellus tempus, lacus. Find out more
+    </TipBox>
+  );
 };
 
 export default WhatsNew;
--- src/components/WhatsNewComponents/WhatsNewHeader.tsx
+++ src/components/WhatsNewComponents/WhatsNewHeader.tsx
@@ -1,16 +1,34 @@
-import { FC } from 'react';
+import { useEffect, useState, FC } from 'react';
 import Button from 'src/components/Button';
 import { HeaderContainer, Header } from './WhatsNew.styles';
+import { WindowWithNoticeableType } from './types';
 
 type Props = {
   title: string;
 };
 
 const WhatsNewHeader: FC<Props> = ({ title }) => {
+  const [hasWindow, setWindow] = useState<boolean>(false);
+
+  useEffect(() => {
+    let intervalId: any;
+    const windowWithNoticeable = window as WindowWithNoticeableType;
+
+    if (windowWithNoticeable.noticeable) {
+      setWindow(true);
+    } else {
+      intervalId = setInterval(() => {
+        if (windowWithNoticeable.noticeable) {
+          setWindow(true);
+        }
+      }, 100);
+    }
+  }, []);
+
   return (
     <HeaderContainer>
       <Header>{title}</Header>
-      <Button as="a" size="large" variant="primary" href="#subscribe">
+      <Button as="a" size="large" variant="primary" href="#subscribe" isDisplayed={hasWindow}>
         Subscribe to updates
       </Button>
     </HeaderContainer>
