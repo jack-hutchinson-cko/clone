e6833ea8adea0e4f0f913d38a8f721c52ab0c777
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-11-10 13:20:34 +0000

    Use webpack 5

--- next.config.js
+++ next.config.js
@@ -9,14 +9,11 @@ const nextConfig = {
   webpack: (config, { isServer }) => {
     // this is where you add custom configuration for webpack
     if (!isServer) {
-      config.node = {
-        fs: 'empty'
-      }
+      config.resolve.fallback.fs = false;
     }
     
     return config;
   },
-  webpack5: false,
   compress: true,
   async redirects() {
     return [
