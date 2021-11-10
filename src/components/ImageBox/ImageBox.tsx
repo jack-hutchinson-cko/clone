4b95bf22fe450d5a86cb69e2bdaeaef0649b4826
Jack Hutchinson <jack.hutchinson@checkout.com>
2021-11-10 15:38:55 +0000

    Set blurdataURL

--- src/components/ImageBox/ImageBox.tsx
+++ src/components/ImageBox/ImageBox.tsx
@@ -4,7 +4,7 @@ import ImgModalWrapper from 'src/components/ImageModalWrapper';
 import { ThemeContext } from 'src/theme/themeContext';
 
 import { generateAltAttribute } from 'src/lib/generateAltAttribute';
-import { ImgWrapper, StyledImage, ContainerImage } from './ImageBoxStyles';
+import { ImgWrapper, StyledImage, ContainerImage, ImgPlaceholder } from './ImageBoxStyles';
 
 const isDarkTheme = 'dark';
 
@@ -43,46 +43,20 @@ const ImageBox: FC<Props> = ({
   ...props
 }) => {
   const { theme } = useContext(ThemeContext);
-  const [placeholderVisibility, setPlaceholderVisibility] = useState(true);
+  const basePath = process.env.NEXT_PUBLIC_CLIENT_TYPE === 'ABC' ? '/docs' : '/docs/four';
   const finalSrc = theme === isDarkTheme && darkThemeSrc ? darkThemeSrc : src;
   const newSrc = encodeURIComponent(finalSrc);
-
-  const toBase64 = (str: string) =>
-    typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str);
-
-  const convertImage = (w: number, h: number) => `
-  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <defs>
-      <linearGradient id="g">
-        <stop stop-color="#333" offset="20%" />
-        <stop stop-color="#222" offset="50%" />
-        <stop stop-color="#333" offset="70%" />
-      </linearGradient>
-    </defs>
-    <rect width="${w}" height="${h}" fill="#333" />
-    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-  </svg>`;
-
-  const onLoadHandler = (event: React.SyntheticEvent<HTMLImageElement>) => {
-    const target = event.target as HTMLImageElement;
-
-    if (target.complete && target.style.visibility !== 'hidden') {
-      setPlaceholderVisibility(false);
-    }
-  };
 
   const imageBox = (
     <ImgWrapper maxWidth={maxWidth}>
       <ContainerImage defaultWidth={defaultWidth} defaultHeight={defaultHeight}>
         <StyledImage
           src={newSrc}
-          onLoad={onLoadHandler}
           loader={loader}
           alt={generateAltAttribute(src)}
           loading={loading}
           placeholder="blur"
-          blurDataURL={`data:image/svg+xml;base64,${toBase64(convertImage(700, 475))}`}
+          blurDataURL={`${basePath}/_next/image?url=${basePath + src}&q=1`}
           {...props}
         />
       </ContainerImage>
